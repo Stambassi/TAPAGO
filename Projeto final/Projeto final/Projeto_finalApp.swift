b0ac062e20be19c141166281ec1bcc07358cf2e2
NayronCampos <nayroncampossoares@gmail.com>
2025-04-04 19:50:05 -0300

    Add files via upload

---
+++ Projeto final/Projeto final/Projeto_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projeto_finalApp.swift
+//  Projeto final
+//
+//  Created by Turma02-25 on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Projeto_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projeto final/Projeto final/TrainingPage.swift
@@ -0,0 +1,20 @@
+//
+//  TrainingPage.swift
+//  Projeto final
+//
+//  Created by Turma02-25 on 04/04/25.
+//
+
+import SwiftUI
+import UIKit
+
+struct TrainingPage: View {
+    
+    var body: some View {
+        Text("AAAAAA")
+    }
+}
+
+#Preview {
+    TrainingPage()
+}
