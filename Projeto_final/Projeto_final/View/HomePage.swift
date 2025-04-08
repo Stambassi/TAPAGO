0f985043611b2638eb02e3b0ad4491a5aa6d60d1
Arthur 'sevak' Costa Serra Negra <163688467+sevak19@users.noreply.github.com>
2025-04-08 20:59:52 -0300

    feat: HomePage

--- Projeto_final/Projeto_final/View/HomePage.swift
+++ Projeto_final/Projeto_final/View/HomePage.swift
@@ -6,11 +6,73 @@
 //
 
 import SwiftUI
+import Charts
 
 struct HomePage: View {
+    
+    struct grafico: Identifiable{
+        var id = UUID().uuidString
+        var day: String
+        var desempenho: Double
+        var tapago: Bool
+    }
+    
+    @State var currentDate: Date = Date()
+    
     var body: some View {
-        Color(.primaryPreset)
-            .ignoresSafeArea()
+        
+        let dados = [grafico(day: "Seg", desempenho: 60.0, tapago: true),
+                     grafico(day: "Ter", desempenho: 90.0, tapago: true),
+                     grafico(day: "Quar", desempenho: 70.0, tapago: true),
+                     grafico(day: "Qui", desempenho: 90.0, tapago: true),
+                     grafico(day: "Sex", desempenho: 100.0, tapago: true)]
+        
+        ZStack {
+            Color(.primaryPreset)
+                .ignoresSafeArea()
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text("Bem vindo, Andre!")
+                            .foregroundColor(.white)
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                    }
+                    HStack {
+                        Text("\(currentDate.formatted(.dateTime.weekday().day().month()))")
+                            .foregroundColor(.white)
+                            .font(.subheadline)
+                        Spacer()
+                    }
+                }.padding()
+                HStack{
+                    ZStack{
+                        Image("retangle").resizable().scaledToFit().frame(width: 340, height: 200).background(.navBar).cornerRadius(30).overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.gray, lineWidth: 1))
+                        Chart{
+                            RuleMark(y: .value("Media", 60))
+                                .foregroundStyle(Color.white)
+                            ForEach(dados){ e in
+                                LineMark(
+                                    x: .value("Index", e.day),
+                                    y: .value("Value", e.desempenho)
+                                )
+                                .interpolationMethod(.catmullRom)
+                                .lineStyle(StrokeStyle(lineWidth: 3))
+                                .foregroundStyle(Color.secondaryPreset)
+                                
+                                PointMark(
+                                    x: .value("Index", e.day),
+                                    y: .value("Value", e.desempenho)
+                                )
+                                .foregroundStyle(Color.secondaryPreset)
+                            }
+                        }
+                        .frame(width: 340, height: 150)
+                    }
+                }
+                NewWorkoutPage()
+            }
+        }
     }
 }
 
